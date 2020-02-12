e45530290cf7daee9b5f0f5e6a6d464b509670f0
Paul <mr.president26rus@gmail.com>
2020-02-12 15:09:00 +0300

    Another code after pushing

--- app/src/main/java/com/example/usinggit/MainActivity.java
+++ app/src/main/java/com/example/usinggit/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     // Connect Firebase to the project
     // Add login future
     // Code after pushing to remote report
+    // Another code after pushing to remote report
 
 }
